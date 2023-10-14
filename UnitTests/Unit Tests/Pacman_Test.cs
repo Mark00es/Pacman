2111109b320ac3b04682397fee9d19f20000f2c4
NicoleUT <130813714+NicoleUT@users.noreply.github.com>
2023-10-13 22:53:31 -0400

    updateCheckDirectionTest

--- UnitTests/Unit Tests/Pacman_Test.cs
+++ UnitTests/Unit Tests/Pacman_Test.cs
@@ -29,17 +29,17 @@ namespace Pacman
             Assert.AreEqual("PacmanImage", pacman.PacmanImage.Name);
         }
           [Test]
-        public void CheckDirectionTest()
+         public void CheckDirectionTest()
         {
             // Initialize Pacman's position
-            pacman.xCoordinate = 5;
-            pacman.yCoordinate = 6;
+            pacman.xCoordinate = 6;
+            pacman.yCoordinate = 5;
 
             // Test the check_direction method for each direction
             Assert.IsTrue(pacman.check_direction(1)); // Check if Pacman can move up (direction 1)
             Assert.IsTrue(pacman.check_direction(2)); // Check if Pacman can move right (direction 2)
-            Assert.IsFalse(pacman.check_direction(3)); // Check if Pacman cant move down (direction 3)
-            Assert.IsFalse(pacman.check_direction(4)); // Check if Pacman cant move left (direction 4)
+            Assert.IsTrue(pacman.check_direction(3)); // Check if Pacman can move down (direction 3)
+            Assert.IsTrue(pacman.check_direction(4)); // Check if Pacman can move left (direction 4)
 
             // Check an invalid direction (should return false)
             Assert.IsFalse(pacman.check_direction(5)); // Check a direction that is not valid (should return false)
