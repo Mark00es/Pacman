4e27614182fd28f251bee45f31b8c0f3abe3d38f
Melina Gutierrez <melina.gutierrez@ucb.edu.bo>
2023-10-17 23:53:03 -0400

    Aumentar pruebas para hometimer_Tick

--- UnitTests/Unit Tests/Ghost_Test.cs
+++ UnitTests/Unit Tests/Ghost_Test.cs
@@ -70,27 +70,122 @@ namespace Pacman.Test
         }
 
         [Test]
-        public void hometimer_Tick_Test()
+        public void Hometimer_Tick_MoveGhostsToHomePositionWhenStateIs2()
         {
             // Arrange
             Ghost ghost = new Ghost();
-            Form formInstance = new Form(); 
+            Form formInstance = new Form(); // Simulamos una instancia de Form
+            ghost.CreateGhostImage(formInstance);
+
+            // Set State to 2 to indicate the ghost is moving home
+            ghost.State[0] = 2;
+
+            // Initialize ghost coordinates, positions, and set positions away from home
+            ghost.xCoordinate[0] = ghost.xStart[0] + 1;
+            ghost.yCoordinate[0] = ghost.yStart[0] + 1;
+            ghost.GhostImage[0].Left = ghost.xStart[0] * 16 - 2; // Not at xpos yet
+            ghost.GhostImage[0].Top = ghost.yStart[0] * 16 + 44;   // Not at ypos yet
+
+            // Act
+            ghost.hometimer_Tick(null, null);
+
+            // Assert
+            // Ensure that the ghost's Left and Top properties are adjusted towards xpos and ypos
+            Assert.AreEqual(ghost.xStart[0] * 16 - 3, ghost.GhostImage[0].Left);
+            Assert.AreEqual(ghost.yStart[0] * 16 + 43, ghost.GhostImage[0].Top);
+            Assert.AreEqual(0, ghost.State[0], "Ghost state should be 0");
+            Assert.AreEqual(ghost.xStart[0], ghost.xCoordinate[0], "Ghost x-coordinate should be at its start");
+            Assert.AreEqual(ghost.yStart[0], ghost.yCoordinate[0], "Ghost y-coordinate should be at its start");
+        }
+
+        [Test]
+        public void Hometimer_Tick_TopLeftIncrease_Test()
+        {
+            Ghost ghost = new Ghost();
+            Form formInstance = new Form(); // Simulamos una instancia de Form
+
             ghost.CreateGhostImage(formInstance);
 
             ghost.State[0] = 2;
+            int xStart = 10; // Configuramos xStart y yStart de acuerdo con tu caso
+            int yStart = 20;
+            ghost.xStart[0] = xStart;
+            ghost.yStart[0] = yStart;
+            int xpos = xStart * 16 - 3;
+            int ypos = yStart * 16 + 43;
 
-            ghost.xCoordinate[0] = ghost.xStart[0] - 1;
-            ghost.yCoordinate[0] = ghost.yStart[0] - 1;
-            ghost.GhostImage[0].Left = ghost.xStart[0] * 16 - 3;
-            ghost.GhostImage[0].Top = ghost.yStart[0] * 16 + 43;
+            ghost.GhostImage[0].Left = xpos + 10; // Configuramos una posición inicial mayor que xpos
+            ghost.GhostImage[0].Top = ypos + 10; // Configuramos una posición inicial mayor que ypos
 
             ghost.hometimer_Tick(null, null);
 
+            Assert.Greater(ghost.GhostImage[0].Left, xpos);
+            Assert.Greater(ghost.GhostImage[0].Top, ypos);
+        }
+
+
+        [Test]
+        public void Hometimer_Tick_NoMovementWhenGhostIsAtHome()
+        {
+            // Arrange
+            Ghost ghost = new Ghost();
+            Form formInstance = new Form(); // Simulamos una instancia de Form
+            ghost.CreateGhostImage(formInstance);
+
+            // Set State to 2 to indicate the ghost is moving home
+            ghost.State[0] = 2;
+
+            // Initialize ghost coordinates, positions, and set positions already at home
+            ghost.xCoordinate[0] = ghost.xStart[0];
+            ghost.yCoordinate[0] = ghost.yStart[0];
+            ghost.GhostImage[0].Left = ghost.xStart[0] * 16 - 3;
+            ghost.GhostImage[0].Top = ghost.yStart[0] * 16 + 43;
+
+            // Store the initial positions
+            int initialLeft = ghost.GhostImage[0].Left;
+            int initialTop = ghost.GhostImage[0].Top;
+
+            // Act
+            ghost.hometimer_Tick(null, null);
+
+            // Assert
+            // Ensure that the ghost's Left and Top properties remain unchanged
+            Assert.AreEqual(initialLeft, ghost.GhostImage[0].Left);
+            Assert.AreEqual(initialTop, ghost.GhostImage[0].Top);
+            Assert.AreEqual(0, ghost.State[0], "Ghost state should be 0");
             Assert.AreEqual(ghost.xStart[0], ghost.xCoordinate[0], "Ghost x-coordinate should be at its start");
             Assert.AreEqual(ghost.yStart[0], ghost.yCoordinate[0], "Ghost y-coordinate should be at its start");
-            Assert.AreEqual(new Point(ghost.xStart[0] * 16 - 3, ghost.yStart[0] * 16 + 43), ghost.GhostImage[0].Location);
-            Assert.AreEqual(0, ghost.State[0], "Ghost state should be 0");
         }
+
+        [Test]
+        public void Hometimer_Tick_MoveGhostsToHomePositionWhenGhostIsSlightlyOffPosition()
+        {
+            // Arrange
+            Ghost ghost = new Ghost();
+            Form formInstance = new Form(); // Simulamos una instancia de Form
+            ghost.CreateGhostImage(formInstance);
+
+            // Set State to 2 to indicate the ghost is moving home
+            ghost.State[0] = 2;
+
+            // Initialize ghost coordinates, positions, and set positions slightly off from home
+            ghost.xCoordinate[0] = ghost.xStart[0];
+            ghost.yCoordinate[0] = ghost.yStart[0];
+            ghost.GhostImage[0].Left = ghost.xStart[0] * 16 - 2; // Not at xpos yet
+            ghost.GhostImage[0].Top = ghost.yStart[0] * 16 + 44;   // Not at ypos yet
+
+            // Act
+            ghost.hometimer_Tick(null, null);
+
+            // Assert
+            // Ensure that the ghost's Left and Top properties are adjusted towards xpos and ypos
+            Assert.AreEqual(ghost.xStart[0] * 16 - 3, ghost.GhostImage[0].Left);
+            Assert.AreEqual(ghost.yStart[0] * 16 + 43, ghost.GhostImage[0].Top);
+            Assert.AreEqual(0, ghost.State[0], "Ghost state should be 0");
+            Assert.AreEqual(ghost.xStart[0], ghost.xCoordinate[0], "Ghost x-coordinate should be at its start");
+            Assert.AreEqual(ghost.yStart[0], ghost.yCoordinate[0], "Ghost y-coordinate should be at its start");
+        }
+
 
     }
 }
